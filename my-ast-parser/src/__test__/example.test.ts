1ba5db50b8be12560ace953146a593c088aaa7a9
xihangzhou <xihangzhou@tencent.com>
2022-11-28 17:28:13 +0800

    feat:  新建项目并且增加调试环境

---
+++ my-ast-parser/src/__test__/example.test.ts
@@ -0,0 +1,8 @@
+// src/__test__/example.test.ts
+import { describe, test, expect } from "vitest";
+
+describe("example test", () => {
+  test("should return correct result", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
